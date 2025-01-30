31681fedadc56a7720484536dcc1a29f0d480b77
jay <jksonar1992@gmail.com>
2025-01-30 18:59:51 +0530

    formate and clean variable file

--- variables.tf
+++ variables.tf
@@ -1,51 +1,61 @@
 variable "Pg_host" {
   default     = "172.16.1.171"
   description = "IP Address of postgresql server"
+  type        = string
 }
 
 variable "Pg_username" {
   default     = "postgres"
   description = "Username of postgres server"
+  type        = string
 }
 
 variable "Pg_port" {
   default     = 5432
   description = "Port of database"
+  type        = number
 }
 
 variable "Pg_password" {
-  sensitive = true
+  sensitive   = true
   default     = "Z4yw26ch"
-  description = "database password"
+  description = "Database password"
+  type        = string
 }
 
 variable "Pg_conn_database" {
   default     = "postgres"
-  description = "database name"
+  description = "Database name"
+  type        = string
 }
 
 variable "Pg_database" {
   default     = "terraform_db"
-  description = "create new database"
+  description = "Create new database"
+  type        = string
 }
 
 variable "Pg_SSLmode" {
   default     = "disable"
   description = "SSL connection of postgres"
+  type        = string
 }
 
 variable "Pg_role_name" {
   default     = "jay"
-  description = "create new role to take access of database"
+  description = "Create new role to take access of database"
+  type        = string
 }
 
 variable "Pg_role_passwd" {
-  sensitive = true
+  sensitive   = true
   default     = "jDR3SK@1s"
-  description = "create new passwd for role"
+  description = "Create new passwd for role"
+  type        = string
 }
 
 variable "Pg_table_name" {
   default     = "users_table"
-  description = "create new table"
+  description = "Create new table"
+  type        = string
 }
